f96a203645e4366132c176cf408ca60053920f09
DevABID <92073265+DevABID@users.noreply.github.com>
2025-06-28 20:44:37 +0600

    Update settings.py2

--- mychat/settings.py
+++ mychat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*qixx-_c-@-_gb9a$ya6vvokzhz%_pmsb=cj33v4260*0ca=#6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['my-blog-django.onrender.com']
+ALLOWED_HOSTS = ['zoom-2-vhyp.onrender.com']
 
 
 # Application definition
